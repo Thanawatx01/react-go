3873a86c031060a2f1534c6702172fca098a6c86
Thanawatx01 <thanawatketiya@gmail.com>
2025-05-30 15:01:50 +0700

    setnew

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,12 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// แปลงจาก import.meta.url ไปเป็น __dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
